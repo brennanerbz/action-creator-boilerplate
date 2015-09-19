3349b93c7e37be4f1c0742039b633eecaddda9a0
Brennan Erbeznik <brennan.erbeznik@gmail.com>
2015-09-19 16:42:33 -0700

    Removed state

--- components/TermRows.js
+++ components/TermRows.js
@@ -2,52 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import TermRow from './TermRow';
 
 export default class TermRows extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      word: '',
-      def: ''
-    }
-  }
-  onWord = (e) => {
-    e.preventDefault();
-    const word = e.target.value;
-    this.setState({
-      word: word
-    });
-  }
-
-  onDef = (e) => {
-    e.preventDefault();
-    const def = e.target.value;
-    this.setState({
-      def: def
-    });
-  }
-
-  submitTerm(id) {
-    const word = this.state.word;
-    const def = this.state.def;
-    console.log(id + word + def)
-  }
-
   render() {
     const { terms } = this.props;
     return (
       <ul>
         {terms.map(term =>
-          <div key={term.id}>
-            <input
-              onChange={this.onWord}
-              placeholder={term.id}/>
-            <input 
-              onChange={this.onDef}
-              placeholder="Definition"/>
-            <button
-              onClick={this.submitTerm.bind(this, term.id)}>
-              Save
-            </button>
-          </div>
+          <TermRow terms={terms} actions={actions}/>
         )}
       </ul>
     );
